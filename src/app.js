f3fd5a06ce0ef57b47d0aaa3932767d7567140a1
Renan Dantas <renancascardodantas@gmail.com>
2020-06-16 21:12:13 -0300

    More changes

--- src/app.js
+++ src/app.js
@@ -34,11 +34,43 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+}
+
+const repository = {
+    id,
+    url,
+    title,
+    techs,
+};
+
+repositories[repositoryIndex] = repository;
+
+return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository not found.'})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
